8e723decc343423dbb288ccda140aae06436498f
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2024-10-31 23:21:37 +0100

    front1

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./Components/Header";
 import MainHeader from "./Components/MainHeader";
 import Qualities from "./Components/Qualities";
+import BotMessage from "./Components/BotMessage";
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <MainHeader />
       <Qualities />
+      <BotMessage/>
     </>
   );
 }
