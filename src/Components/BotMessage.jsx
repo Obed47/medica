df5c89bfa38e2064ca9325612d0967eb4ed26c87
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2024-11-01 20:13:19 +0100

    bot message

--- src/Components/BotMessage.jsx
+++ src/Components/BotMessage.jsx
@@ -4,18 +4,16 @@ import bot from '../assets/bot.png'
 
 const BotMessage = () => {
     return (
-        <div className='container w-100 main-container'>
-            <div className="row gy-4">
-                <div className="col-12 content">
-                    <img src={bot} alt="Medica" className="Bot col-4 col-md-6 w-md-50 w-sm-50" />
-                    <div className="container w-100 pt-3 w-md-100 w-sm-100 col-8 col-md-6 col-sm-6">
-                        <p>
-                            Meet Medica. Your medical prescription assistant.
-                            He’s here to help so you must not stand on the long
-                            waiting lines in hospitals hoping to see a doctor
-                        </p>
-                        <button className="buttonStart">Get Started</button>
-                    </div>
+        <div className='main-container'>
+            <div className="second-container">
+                <img src={bot} alt="Medica" className="Bot" />
+                <div className="container">
+                    <p>
+                        Meet Medica. Your medical prescription assistant.
+                        He’s here to help so you must not stand on the long
+                        waiting lines in hospitals hoping to see a doctor
+                    </p>
+                    <button className="buttonStart">Get Started</button>
                 </div>
             </div>
         </div>
