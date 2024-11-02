a04720bbea698e7429bb915f2542b07ff6487559
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2024-11-02 14:18:20 +0100

    footer elements

--- src/Components/Collaborators.jsx
+++ src/Components/Collaborators.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import DrFigure from './DrFigure';
+import DoctorFigure from './doctorFigure';
 import dr1 from '../assets/dr1.png'
 import dr2 from '../assets/dr2.png'
 import dr3 from '../assets/dr3.png'
@@ -26,22 +26,22 @@ const Collaborators = () => {
             <h1>In Collaboration With Highly Trained Medical personnel</h1>
             <div className="scroll">
                 <div className="figures">
-                    <DrFigure 
+                    <DoctorFigure 
                         background={dr1}
                         name={"Dr Kalera H"}
                         post={"Ophtamologist"}
                     />
-                    <DrFigure 
+                    <DoctorFigure 
                         background={dr4}
                         name={"Dr Kalera H"}
                         post={"Ophtamologist"}
                     />
-                    <DrFigure 
+                    <DoctorFigure 
                         background={dr3}
                         name={"Dr Kalera H"}
                         post={"Ophtamologist"}
                     />
-                    <DrFigure 
+                    <DoctorFigure 
                         background={dr2}
                         name={"Dr Kalera H"}
                         post={"Ophtamologist"}
--- src/Components/doctorFigure.jsx
+++ src/Components/doctorFigure.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './drFigure.css'
+import './doctorFigure.css'
 
-const DrFigure = ({background, name, post}) => {
+const DoctorFigure = ({background, name, post}) => {
 
     const style = {
         backgroundImage: `linear-gradient(rgba(255, 255, 255, 0),rgba(117, 104, 104, 0.342),rgba(24, 24, 24, 0.658)),url(${background})
@@ -22,4 +22,4 @@ const DrFigure = ({background, name, post}) => {
     );
 };
 
-export default DrFigure;
+export default DoctorFigure;
