1e5d2bb508521eebd4c944ef88f77645ea6615f9
AngeLarissa <larissa.bokou@facsciences-uy1.cm>
2024-11-18 06:08:25 +0100

    api consultations

--- consultations/medica/historic_app/models.py
+++ consultations/medica/historic_app/models.py
@@ -9,3 +9,6 @@ class Ordonnance(models.Model):
     precaution = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#Si votre projet nécessite un backend complexe (comme avec Django ou Django REST Framework),
+#P vous devrez peut-être combiner Vercel avec une autre solution pour héberger votre backend, comme Render, Heroku, ou un hébergement VPS.
