0321b1796a3a0e899fe9419018367cf267ebdc15
Obed47 <obedwirngo47@gmail.com>
2024-11-26 01:11:53 +0100

    past consultations component made resusable and adding logic added to the consultations history page

--- src/Components/Consultation/consultationHistory.jsx
+++ src/Components/Consultation/consultationHistory.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import './consultationHistory.css'
-import PastConsultation from './pastConsultation';
-
+import React, { useState } from "react";
+import "./consultationHistory.css";
+import PastConsultation from "./pastConsultation";
 
 const ConsultationHistory = () => {
-    return (
-        <div className='consultationHistory'>
-            <h3>Consultation History</h3>
-            <h5>History about past consultation in <span>read only</span></h5>
-            <PastConsultation/>
-            <PastConsultation/>
-        </div>
-    );
+  const [history, setHistory] = useState([
+    { illnessName: "headache", proposedRemedy: "paracetamol" },
+    { illnessName: "headache", proposedRemedy: "paracetamol" },
+    { illnessName: "headache", proposedRemedy: "paracetamol" },
+    { illnessName: "headache", proposedRemedy: "paracetamol" },
+  ]);
+  return (
+    <div className="consultationHistory">
+      <h3>Consultation History</h3>
+      <h5>
+        History about past consultation in{" "}
+        <span style={{ fontWeight: "bold" }}>read only</span>
+      </h5>
+
+      {history.map((item) => {
+        return (
+          <PastConsultation
+            illness={item.illnessName}
+            remedy={item.proposedRemedy}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default ConsultationHistory;
--- src/Components/Consultation/pastConsultation.jsx
+++ src/Components/Consultation/pastConsultation.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import './pastConsultation.css'
+import React from "react";
+import "./pastConsultation.css";
 
-const PastConsultation = () => {
-    return (
-        <div className='pastConsultation'>
-            <table>
-                <thead>
-                    <td>Ilness: </td>
-                    <td>Proposed Medication</td>
-                    <td> </td>
-                </thead>
-                <tr>
-                    <td>Fever and Headache</td>
-                    <td>Fever and Headache</td>
-                    <td>11/12/24</td>
-                </tr>
-            </table>
-            <button>Delete Icon</button>
-        </div>
-    );
+const PastConsultation = (props) => {
+  const currentDate = new Date();
+  return (
+    <div className="pastConsultation">
+      <table>
+        <thead>
+          <td>Ilness: </td>
+          <td>Proposed Medication</td>
+          <td> </td>
+        </thead>
+        <tr>
+          <td>{props.illness}</td>
+          <td>{props.remedy}</td>
+          <td style={{ display: "flex", flexDirection: "row" }}>
+            {currentDate.getDate()} <p>:</p>
+            {currentDate.getMonth() + 1} <p>:</p> {currentDate.getFullYear()}
+          </td>
+        </tr>
+      </table>
+      <button>Delete Icon</button>
+    </div>
+  );
 };
 
 export default PastConsultation;
