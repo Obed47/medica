a39bb36f4724ace7d4cea59cf766e925be9464f0
Obed47 <obedwirngo47@gmail.com>
2024-12-17 17:20:33 +0100

    bundled the welcomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./Components/Header";
-import MainHeader from "./Components/MainHeader";
-import Qualities from "./Components/Qualities";
-import BotMessage from "./Components/BotMessage";
-import AllRounds from "./Components/allrouds";
-import Collaborators from "./Components/Collaborators";
-
-import Footer from "./Components/Footer";
+import WelcomePage from "./welcomePage";
 
 function App() {
   return (
     <>
       {/* note that this is just to test. We will rende these components conditionally */}
-      <Header />
-      <MainHeader />
-      <Qualities />
-      <AllRounds />
-      <BotMessage />
-      <Collaborators />
-      <Footer />
+      <WelcomePage />
     </>
   );
 }
